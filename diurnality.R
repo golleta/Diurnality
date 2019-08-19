944c7aa5bcfd62002a51bd48ee7ff110aff98834
Jefferson Silva <jfo.txt@gmail.com>
2019-08-19 19:36:12 -0300

    - changed argument names (again) - changed required package suncalc to maptools - daylenght using maptools now accounts for dusk and dawn

--- diurnality.R
+++ diurnality.R
@@ -3,40 +3,41 @@
 # Jefferson Silva
 ###################################
 
-diurnality = function(timestamp, activity, interval = 0, lat = -28.8, lon = -66.95, sunrise = NULL, sunset = NULL, graph = FALSE){
+diurnality = function(datetime, activity, interval = 0, lat = -28.8, lon = -66.95, sunrise = NULL, sunset = NULL, graph = FALSE){
 	
 	# checa se pacotes necessários estão instalados
-	if (!require(suncalc)){
+	require(lubridate)
+	# checa se pacotes necessários estão instalados
+	if (!require(maptools)){
 		# Instala pacotes caso não estejam instalados
-		install.packages("suncalc")
+		install.packages("maptools")
 	}else
 	{
 		# carrega pacotes necessários
 		# suncalc é usado para calcular a duração do dia em determinado dia do ano dado as coordenadas do local
-		require(suncalc)
+		require(maptools)
 	}
-	
 	#####################
 	# Verifica argumentos
 	#####################
 	
 	#### Datetime deve ser do tipo POSIXct para evitar erros futuros
-	if(!inherits(timestamp, "POSIXct")){
+	if(!inherits(datetime, "POSIXct")){
 		# caso não seja a função é parada
-		stop("Argumento 'timestamp' deve ser da classe POSIXct")
+		stop("Argumento 'datetime' deve ser da classe POSIXct")
 	}
 	
 	# Intervalo para calculo do indice deve ser > 1
-	if(interval<=0){
-	  stop("Argumento 'interval' deve ser >= 1")
+	if(interval<0){
+	  stop("Argumento 'interval' deve ser positivo")
 	}
 	
 	if(!is.numeric(activity)){
 		stop("Argumento 'activity' deve ser numérico.")
 	}
 	
-	if(length(activity) != length(timestamp)){
-		stop("Argumentos 'timestamp' e 'activity'devem ter o mesmo tamanho.")
+	if(length(activity) != length(datetime)){
+		stop("Argumentos 'datetime' e 'activity'devem ter o mesmo tamanho.")
 	}
 	
 	#### Devem ser preenchido (sunrise e sunset) ou então (lat, lon)
@@ -77,7 +78,7 @@ diurnality = function(timestamp, activity, interval = 0, lat = -28.8, lon = -66.
 	####################
 	
 	# Combina dados de entrada em um dataframe, o que facilita a manipulação.
-	df = data.frame(timestamp, activity)
+	df = data.frame(datetime, activity)
 	# omite NAs
 	df = na.omit(df)
 	
@@ -86,25 +87,30 @@ diurnality = function(timestamp, activity, interval = 0, lat = -28.8, lon = -66.
 		## Esse bloco de código cria uma nova coluna no dataframe
 		## A nova coluna 'daylight' indica que o registro correspondente aquela linha foi realizado durante o dia
 		# Para cada linha extrai somente o valor da data, descartando as horas
-		dates = as.Date(df$timestamp)
+		date = as_date(df$datetime)
 		# concatena a data correspondete daquela linha com o horário de nascer e pôr do sol
-		sunrise = paste(dates, sunrise)
-		sunset = paste(dates, sunset)
+		sunrise = ymd_hm(paste(date, sunrise))
+		sunset = ymd_hm(paste(date, sunset))
 		# Verifica se o registro foi feito entre as horas de nascer e por do sol e adiciona a nova coluna 'daylight' ao dataframe
-		df$daylight = ifelse(test = df$timestamp >= sunrise & df$timestamp <= sunset, yes = TRUE, no = FALSE)
+		df$daylight = ifelse(test = df$datetime >= sunrise & df$datetime <= sunset, yes = TRUE, no = FALSE)
 		
 	}	
 	else{
-		## Para conseguir dados de nascer e por do sol, temos três opções em três pacotes diferentes: 
-		## suncalc::getSunlightTimes, maptools::sunriset e StreamMetabolism::sunrise.set
-		## Em tempo de execuçao suncalc::getSunlightTimes foi mais rápido do que as outras funçoes
-		# cria novo vetor com as datas de nasce e por do sol para cada linha do df
-		sun = getSunlightTimes(as.Date(df$timestamp), lat = lat, lon = lon, keep = c("sunrise", "sunset"))
+		## O pacote 'maptools' é usado para conseguir a duração do dia automaticamente
+		# As coordenadas devem ser convertidas em uma matriz
+		crds = matrix(c(lon, lat), nrow = 1)
+		
+		# calcula horarios de inicio dos crepusulos (dawn e dusk)
+		# Qual angulo solar usar? https://www.timeanddate.com/astronomy/different-types-twilight.html
+		dawn <- crepuscule(crds = crds, dateTime = df$datetime, solarDep = 6, direction = "dawn", POSIXct.out=TRUE)$time
+		dusk <- crepuscule(crds = crds, dateTime = df$datetime, solarDep = 6, direction = "dusk", POSIXct.out=TRUE)$time
+		
 		# Verifica se df está entre as horas de nascer e por do sol e adiciona a nova coluna 'daylight' ao dataframe.
-		df$daylight = ifelse(test = df$timestamp >= sun$sunrise & df$timestamp <= sun$sunset, yes = TRUE, no = FALSE)
+		df$daylight = ifelse(test = df$datetime  >= dawn & df$datetime  <= dusk, yes = TRUE, no = FALSE)
 	}
 	
-	
+	## Se o intervalo é maior ou igual a 0 o indice é calculado para toda atividade do dataset fornecido
+	## Caso o intervalo seja maior ou igual a 1 o indice é calculado por grupo de dias
 	if (interval == 0){
 		# indexa valores de atividade que acontecem durante o dia
 		actv.day = df$activity[df$daylight]
@@ -121,7 +127,7 @@ diurnality = function(timestamp, activity, interval = 0, lat = -28.8, lon = -66.
 		# Cria uma string de acordo com o intervalo de dias fornecido nos argumentos
 		b = paste(as.character(interval),"days")
 		# Cria um fator que corresponde ao intervalo de cada uma das linhas do df.
-		cuts = cut(x = df$timestamp, breaks = b)
+		cuts = cut(x = df$datetime, breaks = b)
 		#  Cria uma lista dividida por intervalos
 		df.list = split(x = df, f = cuts)
 		# Separa atividade que acontece durante dia ou noite e calcula o indice de diurnalidade (Hoogenboom, 1984)
@@ -137,6 +143,7 @@ diurnality = function(timestamp, activity, interval = 0, lat = -28.8, lon = -66.
 						 	actv.night = sum(actv.night)
 						 	# calculo do indice de diurnalidade (Hoogenboom, 1984)
 						 	d = (actv.day - actv.night)/(actv.day + actv.night)
+						 	return(d)
 						 })
 		d.index = data.frame(date = names(d.index), diurnality = d.index, row.names = NULL)
 	}
